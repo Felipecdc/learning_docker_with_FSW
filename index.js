a2bf834f284ecac38f4c0b1f2b68e54a33e64aa6
Felipe Castro <felipecdc09@gmail.com>
2024-08-16 20:58:37 -0300

    chore: add pg and dotenv setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const name = "";
+import "dotenv/config.js";
+import express from "express";
+
+import { PostgresHelper } from "./src/db/postgres/helper.js";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    const result = await PostgresHelper.query("SELECT * FROM users;");
+
+    res.send(JSON.stringify(result));
+});
+
+app.listen(3000, () => console.log("Started DB"));
