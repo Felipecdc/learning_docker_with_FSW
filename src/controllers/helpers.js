440f9f59124638b12ed48da4ebb9fce042252a3a
Felipe Castro <felipecdc09@gmail.com>
2024-08-18 11:10:09 -0300

    feat: add get user by id controller and validations

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -1,19 +1,17 @@
-export const badRequest = (body) => {
+export const badRequest = (body) => ({ statusCode: 400, body });
+
+export const created = (body) => ({ statusCode: 201, body });
+
+export const serverError = () => ({
+    statusCode: 500,
+    body: {
+        message: "Internal Server Error",
+    },
+});
+
+export const ok = (body) => {
     return {
-        statusCode: 400,
+        statusCode: 200,
         body,
-    };
-};
-
-export const created = (body) => {
-    return { statusCode: 201, body };
-};
-
-export const serverError = () => {
-    return {
-        statusCode: 500,
-        body: {
-            message: "Internal Server Error",
-        },
     };
 };
