fc23081b5f4ae98be4c09653330e457b014dc259
Felipe Castro <felipecdc09@gmail.com>
2024-08-19 19:35:00 -0300

    ffeat: ensure 404 is returned if no user id found

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -1,5 +1,7 @@
 export const badRequest = (body) => ({ statusCode: 400, body });
 
+export const notFound = (body) => ({ statusCode: 404, body });
+
 export const created = (body) => ({ statusCode: 201, body });
 
 export const serverError = () => ({
