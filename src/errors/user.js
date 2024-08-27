84b4328232426a6c16b8b3fee6e3ebf4c45e8de2
Felipe Castro <felipecdc09@gmail.com>
2024-08-27 19:36:09 -0300

    feat: add createTransactionUseCase

--- src/errors/user.js
+++ src/errors/user.js
@@ -4,3 +4,10 @@ export class EmailALreadyInUseError extends Error {
         this.name = "EmailALreadyInUseError";
     }
 }
+
+export class UserNotFoundError extends Error {
+    constructor(userId) {
+        super(`User with id ${userId} not found.`);
+        this.name = "UserNotFoundError";
+    }
+}
