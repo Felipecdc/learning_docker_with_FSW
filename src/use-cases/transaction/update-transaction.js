fb52831fe7008f8572fc4c6ec26efc3d0974991f
Felipe Castro <felipecdc09@gmail.com>
2024-09-02 15:36:32 -0300

    fix: pass correct params to updateTransactionRepository

--- src/use-cases/transaction/update-transaction.js
+++ src/use-cases/transaction/update-transaction.js
@@ -1,18 +1,10 @@
-import { UserNotFoundError } from "../../errors/user.js";
-
 export class UpdateTransactionUseCase {
-    constructor(updateTransactionRepository, getUserByIdRepository) {
+    constructor(updateTransactionRepository) {
         this.updateTransactionRepository = updateTransactionRepository;
-        this.getUserByIdRepository = getUserByIdRepository;
     }
-    async execute(params) {
-        const userId = await this.getUserByIdRepository.execute(params.userId);
-
-        if (!userId) {
-            throw new UserNotFoundError();
-        }
-
+    async execute(transactionId, params) {
         const transaction = await this.updateTransactionRepository.execute(
+            transactionId,
             params
         );
 
